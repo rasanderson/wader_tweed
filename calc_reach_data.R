ced62104f8d72de6923c9ba36f0e4b5b00b4faa1
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2023-08-18 17:15:05 +0100

    Added data creation for Reach 7 to 12

--- calc_reach_data.R
+++ calc_reach_data.R
@@ -27,12 +27,13 @@ reach_length <- c(7000, 7000, 7000, 7500, 6000, 4500, 8750, 8500, 6000,
                   6500, 8000, 8000, 4000, 6000)
 plot(fig_lengths, reach_length[2:23]/1000) # very close match
 
+# Gauged Reach 6 ####
 # Use the percentage decline of flow from guaged downstream reaches to aid
 # calibration of ungauged upstream ones. e.g. reach 1 has predicted 4.849
 # flow vs 27.153, from fitted(flow_lm) i.e. 17.8% of flow. Take the airGR
 # OutputsModel$Qsim values or the observed reach 6 guaged values to do
 # initial calibration of reach 1 etc.
-flow_pct_diff <- data.frame(reach_no=1:6, pct=NA)
+flow_pct_diff <- data.frame(reach_no=1:23, pct=NA)
 guaged_reach_no <- 6
 for(i in 1:6){
   flow_pct_diff$pct[i] <- fitted(flow_lm)[i] / fitted(flow_lm)[guaged_reach_no]
@@ -89,12 +90,11 @@ for(month_no in 1:no_of_months){
 BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
 saveRDS(BasinObs, file = paste0("data/BasinObs_", guaged_reach_no, ".RDS"))
 
+# Ungauged Reaches 1 to 5 ####
 # Repeat the process for the ungaugaed reaches 1 to 5
 # Adjust the Qmm in accordance with values from earlier linear model
 # Need to put all this in a function or simplify at some point
 # As it is so slow will try as a function and parallise it (Linux-only)
-
-
 for(reach_no in 1:5){
   print(paste0("Reach number:", reach_no))
   inca_sub <- inca23[inca23$reach_no == reach_no,]
@@ -142,7 +142,7 @@ for(reach_no in 1:5){
 # as default in airGR semi-distributed model.
 delay_days <- 2
 
-
+# Gauged Reach 7 ####
 # Reach 7 Peebles / Scots Mill is gauged
 # ID 21003
 guaged_reach_no <- 7
@@ -188,4 +188,103 @@ for(month_no in 1:no_of_months){
 }
 BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
 saveRDS(BasinObs, file = paste0("data/BasinObs_", guaged_reach_no, ".RDS"))
+
+# Gauged Reach 12  ####
+# Reach 12 Boleside is gauged
+# ID 21006
+guaged_reach_no <- 12
+stn_gdf <- gdf(id = "21006")
+inca23 <- read_sf("data/inca23.gpkg")
+inca_sub <- inca23[inca23$reach_no == guaged_reach_no,]
+plot(inca_sub["reach_no"])
+
+rain_files <- list.files("data/chess-met_precip/", full.names = TRUE)
+pet_files  <- list.files("data/chess-pe_pet/", full.names = TRUE)
+
+no_of_months <- length(rain_files)
+
+BasinObs <- data.frame(DatesR = as.Date(character()), P = numeric(), E = numeric(),  Qmm = numeric())
+
+# Next bit is slow
+for(month_no in 1:no_of_months){
+  print(round(month_no / no_of_months * 100), 2)
+  rain <- terra::rast(rain_files[month_no])  
+  pet  <- terra::rast(pet_files[month_no])
+  
+  terra::crs(rain) <- terra::crs("+init=epsg:27700")
+  terra::crs(pet) <- terra::crs("+init=epsg:27700")
+  
+  for(day in 1:dim(rain)[3]){
+    cat(".")
+    rain_1day <- rain[[day]]
+    pet_1day  <- pet[[day]]
+    # day_rain_sub <- terra::extract(rain_1day, vect(inca_sub["reach_no"]))
+    # day_pet_sub  <- terra::extract(pet_1day,  vect(inca_sub["reach_no"]))
+    # day_rain_sub <- crop(mask(rain_1day, vect(inca_sub)), vect(inca_sub)) # returns map of just target area
+    day_rain_sub <- terra::extract(rain_1day, terra::vect(inca_sub))
+    day_pet_sub  <- terra::extract(pet_1day,  terra::vect(inca_sub))
+    daily_rain_mean <- mean(day_rain_sub[,2]) * 24 * 60 * 60 # Convert to mm / day
+    daily_pet_mean  <- mean(day_pet_sub[,2])
+    rain_stats <- data.frame(DatesR = terra::time(rain_1day),
+                             P      = daily_rain_mean,
+                             E      = daily_pet_mean,
+                             Qmm    = stn_gdf[terra::time(rain_1day)])
+    BasinObs <- rbind(BasinObs, rain_stats)
+  }
+  
+}
+BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
+saveRDS(BasinObs, file = paste0("data/BasinObs_", guaged_reach_no, ".RDS"))
+
+# Ungauged Reaches 8 to 11 ####
+# Estimate pct decline in flow upstream of Reach 12 to ungauged Reach 8
+guaged_reach_no <- 12
+for(i in 8:12){
+  flow_pct_diff$pct[i] <- fitted(flow_lm)[i] / fitted(flow_lm)[guaged_reach_no]
+}
+# Ungauged Reaches 8 to 11
+# Repeat the process for the ungaugaed reaches
+# Adjust the Qmm in accordance with values from earlier linear model
+# Need to put all this in a function or simplify at some point
+# As it is so slow will try as a function and parallise it (Linux-only)
+for(reach_no in 8:11){
+  print(paste0("Reach number:", reach_no))
+  inca_sub <- inca23[inca23$reach_no == reach_no,]
+  plot(inca_sub["reach_no"])
+  
+  BasinObs <- data.frame(DatesR = as.Date(character()), P = numeric(), E = numeric(),  Qmm = numeric())
+  
+  # Next bit is slow
+  #for(month_no in 1:no_of_months-82){
+  #  print(round(month_no / no_of_months * 100), 2)
+  for(month_no in 1:no_of_months){
+    print(round(month_no / no_of_months * 100), 2)
+    rain <- terra::rast(rain_files[month_no])  
+    pet  <- terra::rast(pet_files[month_no])
+    
+    terra::crs(rain) <- terra::crs("+init=epsg:27700")
+    terra::crs(pet) <- terra::crs("+init=epsg:27700")
+    
+    # for(day in 1:dim(rain)[3]-22){
+    #   tic("day loop")
+    #   #cat(".")
+    for(day in 1:dim(rain)[3]){
+      cat(".")
+      rain_1day <- rain[[day]]
+      pet_1day  <- pet[[day]]
+      day_rain_sub <- terra::extract(rain_1day, terra::vect(inca_sub))
+      day_pet_sub  <- terra::extract(pet_1day,  terra::vect(inca_sub))
+      daily_rain_mean <- mean(day_rain_sub[,2]) * 24 * 60 * 60 # Convert to mm / day
+      daily_pet_mean  <- mean(day_pet_sub[,2])
+      rain_stats <- data.frame(DatesR = terra::time(rain_1day),
+                               P      = daily_rain_mean,
+                               E      = daily_pet_mean,
+                               Qmm    = stn_gdf[terra::time(rain_1day)] * flow_pct_diff$pct[reach_no])
+      BasinObs <- data.table::rbindlist(list(BasinObs, rain_stats))
+    }   
+  }
+  BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
+  saveRDS(BasinObs, file = paste0("data/BasinObs_", reach_no, ".RDS"))
+}
+
 
