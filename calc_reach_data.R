9eec443d349da0aa5694ef3c6b61ae30273445e0
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2023-08-21 15:34:08 +0100

    Calculations for RDS files for remaining reaches added

--- calc_reach_data.R
+++ calc_reach_data.R
@@ -287,4 +287,192 @@ for(reach_no in 8:11){
   saveRDS(BasinObs, file = paste0("data/BasinObs_", reach_no, ".RDS"))
 }
 
+# Reach 19 gauged ####
+# Sprouston 21021
+guaged_reach_no <- 19
+stn_gdf <- gdf(id = "21021")
+inca23 <- read_sf("data/inca23.gpkg")
+inca_sub <- inca23[inca23$reach_no == guaged_reach_no,]
+plot(inca_sub["reach_no"])
 
+rain_files <- list.files("data/chess-met_precip/", full.names = TRUE)
+pet_files  <- list.files("data/chess-pe_pet/", full.names = TRUE)
+
+no_of_months <- length(rain_files)
+
+BasinObs <- data.frame(DatesR = as.Date(character()), P = numeric(), E = numeric(),  Qmm = numeric())
+
+# Next bit is slow
+for(month_no in 1:no_of_months){
+  print(round(month_no / no_of_months * 100), 2)
+  rain <- terra::rast(rain_files[month_no])  
+  pet  <- terra::rast(pet_files[month_no])
+  
+  terra::crs(rain) <- terra::crs("+init=epsg:27700")
+  terra::crs(pet) <- terra::crs("+init=epsg:27700")
+  
+  for(day in 1:dim(rain)[3]){
+    cat(".")
+    rain_1day <- rain[[day]]
+    pet_1day  <- pet[[day]]
+    # day_rain_sub <- terra::extract(rain_1day, vect(inca_sub["reach_no"]))
+    # day_pet_sub  <- terra::extract(pet_1day,  vect(inca_sub["reach_no"]))
+    # day_rain_sub <- crop(mask(rain_1day, vect(inca_sub)), vect(inca_sub)) # returns map of just target area
+    day_rain_sub <- terra::extract(rain_1day, terra::vect(inca_sub))
+    day_pet_sub  <- terra::extract(pet_1day,  terra::vect(inca_sub))
+    daily_rain_mean <- mean(day_rain_sub[,2]) * 24 * 60 * 60 # Convert to mm / day
+    daily_pet_mean  <- mean(day_pet_sub[,2])
+    rain_stats <- data.frame(DatesR = terra::time(rain_1day),
+                             P      = daily_rain_mean,
+                             E      = daily_pet_mean,
+                             Qmm    = stn_gdf[terra::time(rain_1day)])
+    BasinObs <- rbind(BasinObs, rain_stats)
+  }
+  
+}
+BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
+saveRDS(BasinObs, file = paste0("data/BasinObs_", guaged_reach_no, ".RDS"))
+
+# Reaches 13 to 18 ungauged ####
+# Estimate pct decline in flow upstream of Reach 19 to ungauged Reach 13
+guaged_reach_no <- 19
+for(i in 13:18){
+  flow_pct_diff$pct[i] <- fitted(flow_lm)[i] / fitted(flow_lm)[guaged_reach_no]
+}
+# Ungauged Reaches 13 to 18
+# Repeat the process for the ungaugaed reaches
+# Adjust the Qmm in accordance with values from earlier linear model
+for(reach_no in 13:18){
+  print(paste0("Reach number:", reach_no))
+  inca_sub <- inca23[inca23$reach_no == reach_no,]
+  plot(inca_sub["reach_no"])
+  
+  BasinObs <- data.frame(DatesR = as.Date(character()), P = numeric(), E = numeric(),  Qmm = numeric())
+  
+  # Next bit is slow
+  #for(month_no in 1:no_of_months-82){
+  #  print(round(month_no / no_of_months * 100), 2)
+  for(month_no in 1:no_of_months){
+    print(round(month_no / no_of_months * 100), 2)
+    rain <- terra::rast(rain_files[month_no])  
+    pet  <- terra::rast(pet_files[month_no])
+    
+    terra::crs(rain) <- terra::crs("+init=epsg:27700")
+    terra::crs(pet) <- terra::crs("+init=epsg:27700")
+    
+    # for(day in 1:dim(rain)[3]-22){
+    #   tic("day loop")
+    #   #cat(".")
+    for(day in 1:dim(rain)[3]){
+      cat(".")
+      rain_1day <- rain[[day]]
+      pet_1day  <- pet[[day]]
+      day_rain_sub <- terra::extract(rain_1day, terra::vect(inca_sub))
+      day_pet_sub  <- terra::extract(pet_1day,  terra::vect(inca_sub))
+      daily_rain_mean <- mean(day_rain_sub[,2]) * 24 * 60 * 60 # Convert to mm / day
+      daily_pet_mean  <- mean(day_pet_sub[,2])
+      rain_stats <- data.frame(DatesR = terra::time(rain_1day),
+                               P      = daily_rain_mean,
+                               E      = daily_pet_mean,
+                               Qmm    = stn_gdf[terra::time(rain_1day)] * flow_pct_diff$pct[reach_no])
+      BasinObs <- data.table::rbindlist(list(BasinObs, rain_stats))
+    }   
+  }
+  BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
+  saveRDS(BasinObs, file = paste0("data/BasinObs_", reach_no, ".RDS"))
+}
+
+# Reach 23 gauged ####
+# Noreham 21009
+guaged_reach_no <- 23
+stn_gdf <- gdf(id = "21009")
+inca23 <- read_sf("data/inca23.gpkg")
+inca_sub <- inca23[inca23$reach_no == guaged_reach_no,]
+plot(inca_sub["reach_no"])
+
+rain_files <- list.files("data/chess-met_precip/", full.names = TRUE)
+pet_files  <- list.files("data/chess-pe_pet/", full.names = TRUE)
+
+no_of_months <- length(rain_files)
+
+BasinObs <- data.frame(DatesR = as.Date(character()), P = numeric(), E = numeric(),  Qmm = numeric())
+
+# Next bit is slow
+for(month_no in 1:no_of_months){
+  print(round(month_no / no_of_months * 100), 2)
+  rain <- terra::rast(rain_files[month_no])  
+  pet  <- terra::rast(pet_files[month_no])
+  
+  terra::crs(rain) <- terra::crs("+init=epsg:27700")
+  terra::crs(pet) <- terra::crs("+init=epsg:27700")
+  
+  for(day in 1:dim(rain)[3]){
+    cat(".")
+    rain_1day <- rain[[day]]
+    pet_1day  <- pet[[day]]
+    # day_rain_sub <- terra::extract(rain_1day, vect(inca_sub["reach_no"]))
+    # day_pet_sub  <- terra::extract(pet_1day,  vect(inca_sub["reach_no"]))
+    # day_rain_sub <- crop(mask(rain_1day, vect(inca_sub)), vect(inca_sub)) # returns map of just target area
+    day_rain_sub <- terra::extract(rain_1day, terra::vect(inca_sub))
+    day_pet_sub  <- terra::extract(pet_1day,  terra::vect(inca_sub))
+    daily_rain_mean <- mean(day_rain_sub[,2]) * 24 * 60 * 60 # Convert to mm / day
+    daily_pet_mean  <- mean(day_pet_sub[,2])
+    rain_stats <- data.frame(DatesR = terra::time(rain_1day),
+                             P      = daily_rain_mean,
+                             E      = daily_pet_mean,
+                             Qmm    = stn_gdf[terra::time(rain_1day)])
+    BasinObs <- rbind(BasinObs, rain_stats)
+  }
+  
+}
+BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
+saveRDS(BasinObs, file = paste0("data/BasinObs_", guaged_reach_no, ".RDS"))
+
+# Reaches 20 to 22 ungauged
+# Estimate pct decline in flow upstream of Reach 19 to ungauged Reach 13
+guaged_reach_no <- 23
+for(i in 20:22){
+  flow_pct_diff$pct[i] <- fitted(flow_lm)[i] / fitted(flow_lm)[guaged_reach_no]
+}
+# Ungauged Reaches 20 to 22
+# Repeat the process for the ungaugaed reaches
+# Adjust the Qmm in accordance with values from earlier linear model
+for(reach_no in 20:22){
+  print(paste0("Reach number:", reach_no))
+  inca_sub <- inca23[inca23$reach_no == reach_no,]
+  plot(inca_sub["reach_no"])
+  
+  BasinObs <- data.frame(DatesR = as.Date(character()), P = numeric(), E = numeric(),  Qmm = numeric())
+  
+  # Next bit is slow
+  #for(month_no in 1:no_of_months-82){
+  #  print(round(month_no / no_of_months * 100), 2)
+  for(month_no in 1:no_of_months){
+    print(round(month_no / no_of_months * 100), 2)
+    rain <- terra::rast(rain_files[month_no])  
+    pet  <- terra::rast(pet_files[month_no])
+    
+    terra::crs(rain) <- terra::crs("+init=epsg:27700")
+    terra::crs(pet) <- terra::crs("+init=epsg:27700")
+    
+    # for(day in 1:dim(rain)[3]-22){
+    #   tic("day loop")
+    #   #cat(".")
+    for(day in 1:dim(rain)[3]){
+      cat(".")
+      rain_1day <- rain[[day]]
+      pet_1day  <- pet[[day]]
+      day_rain_sub <- terra::extract(rain_1day, terra::vect(inca_sub))
+      day_pet_sub  <- terra::extract(pet_1day,  terra::vect(inca_sub))
+      daily_rain_mean <- mean(day_rain_sub[,2]) * 24 * 60 * 60 # Convert to mm / day
+      daily_pet_mean  <- mean(day_pet_sub[,2])
+      rain_stats <- data.frame(DatesR = terra::time(rain_1day),
+                               P      = daily_rain_mean,
+                               E      = daily_pet_mean,
+                               Qmm    = stn_gdf[terra::time(rain_1day)] * flow_pct_diff$pct[reach_no])
+      BasinObs <- data.table::rbindlist(list(BasinObs, rain_stats))
+    }   
+  }
+  BasinObs$DatesR <- as.POSIXct(BasinObs$DatesR)
+  saveRDS(BasinObs, file = paste0("data/BasinObs_", reach_no, ".RDS"))
+}
